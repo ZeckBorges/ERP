b5b2df110bd500b747f18a08487b8f5423dc0368
ZeckBorges <quielborges@gmail.com>
2024-11-28 13:39:30 -0300

    Atualização de informações

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -7,6 +7,7 @@ export const listProducts = async (req: Request, res: Response) => {
         res.json({produtos})
     } catch (error) {
         res.status(500).json({ message: 'Erro ao buscar dados!!', error });
+        console.log(error)
     }
         
 }
--- src/db/database.ts
+++ src/db/database.ts
@@ -1,17 +1,58 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize({
+/*const sequelize = new Sequelize('TESTE', 'Ezequiel', '2468', {
+  host: 'localhost',
   dialect: 'mssql',
-  host: 'localhost', // Altere para o host do SQL Server
-  database: 'TESTE', // O nome do seu banco de dados
   dialectOptions: {
+    authentication: {
+      type: 'ntlm',
+      options: {
+        domain: '', // Se necessário
+        userName: '',         // Deixe vazio para Trusted Connection
+        password: '',         // Deixe vazio para Trusted Connection
+      },
+    },
     options: {
-      trustedConnection: true, // Usar a autenticação do Windows
+      instanceName: 'NTB-EZEQUIEL',
+      trustedConnection: true,
+      encrypt: false,
+      requestTimeout: 30000,
     },
   },
-  logging: false, // Opcional, desativa logs no console
-  username: '', // Deixe vazio para autenticação do Windows
-  password: '', // Deixe vazio para autenticação do Windows
+  port: 1433,
+});*/
+
+
+// Note that you must use a SQL Server login -- Windows credentials will not work.
+const sequelize = new Sequelize('TESTE ', 'user', '1234', {
+    dialect: 'mssql',
+    host: 'NTB-EZEQUIEL',
+    port: 1433, // Default port
+    dialectOptions: {
+        instanceName: 'SQLEXPRESS',
+        requestTimeout: 30000,
+        options: {
+          encrypt: true,  // Habilita criptografia
+          trustServerCertificate: true  // Ignora a verificação de certificado
+      }
+    },
+    pool: {
+        max: 50,
+        min: 0,
+        idle: 10000
+    }
 });
+
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão bem-sucedida!');
+  } catch (error) {
+    console.error('Erro ao conectar:', error);
+  }
+})();
+
+console.log(sequelize)
 
 export default sequelize;
