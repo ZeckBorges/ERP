b65284995a3722ac681c2826128ddd9d62a3d952
ZeckBorges <quielborges@gmail.com>
2024-12-03 13:36:29 -0300

    Conexão com sucesso ao banco de dados

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,4 +1,26 @@
 import { Sequelize } from 'sequelize';
+//import dotenv from 'dotenv';
+
+/*dotenv.config(); // Carrega as variáveis do arquivo .env
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  dialect: 'mssql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  dialectOptions: {
+    instanceName: process.env.DB_INSTANCE || '', // Se necessário
+    options: {
+      encrypt: true,
+      trustServerCertificate: true,
+      requestTimeout: 30000,
+    },
+  },
+  pool: {
+    max: 50,
+    min: 0,
+    idle: 10000,
+  },
+});*/
 
 /*const sequelize = new Sequelize('TESTE', 'Ezequiel', '2468', {
   host: 'localhost',
