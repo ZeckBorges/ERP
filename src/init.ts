a435ce82946174db36853b8c3dbe4fc7a957fdc8
ZeckBorges <quielborges@gmail.com>
2024-12-06 16:38:49 -0300

    Atualização do nome da pasta e importações

--- src/init.ts
+++ src/init.ts
@@ -1,6 +1,6 @@
 // src/init.ts
 import { init as initUsers } from './modules/users/routes/usersRoute';
-import { init as initProducts } from './modules/produtcs/routes/productRoute';
+import { init as initProducts } from './modules/products/routes/productRoute';
 
 export const init = (expressInstance: any, basePath: string) => {
   initUsers(expressInstance, basePath);
